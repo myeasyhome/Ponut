171b1bda688d815da2e29ec181f4b326c68b2863
Clivern <hello@clivern.com>
2017-07-22 21:34:43 +0200

    add plugins dir

---
+++ plugins/hello/hello.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Ponut - Applicant Tracking System
+ *
+ * @author      Clivern <hello@clivern.com>
+ * @link        http://ponut.co
+ * @license     MIT
+ * @package     Ponut
+ */
+
+include_once dirname(__FILE__) . 'core.php';
+
+core::instance()->init();
