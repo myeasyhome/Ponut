7adfef2d233de712c368f47a12d3b4e4a67add4a
Clivern <hello@clivern.com>
2017-07-22 21:20:32 +0200

    App namespace updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Ponut\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
--- config/app.php
+++ config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        Ponut\Providers\AppServiceProvider::class,
+        Ponut\Providers\AuthServiceProvider::class,
+        // Ponut\Providers\BroadcastServiceProvider::class,
+        Ponut\Providers\EventServiceProvider::class,
+        Ponut\Providers\RouteServiceProvider::class,
 
     ],
 
