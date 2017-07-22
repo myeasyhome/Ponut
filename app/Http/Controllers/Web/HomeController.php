10694723a8f387a67291e9c475391ea64116adb1
Clivern <hello@clivern.com>
2017-07-22 23:49:47 +0200

    add our first controllers home & setup api

---
+++ app/Http/Controllers/Web/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Ponut - Applicant Tracking System
+ *
+ * @author      Clivern <hello@clivern.com>
+ * @link        http://ponut.co
+ * @license     MIT
+ * @package     Ponut
+ */
+
+namespace Ponut\Http\Controllers\Web;
+
+use Ponut\Http\Controllers\Controller;
+use Ponut\Models\Option;
+use Validator;
+use Input;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class HomeController extends Controller
+{
+
+}
