7393bd24a59393832611852d80fd0dd51f118c0b
Clivern <hello@clivern.com>
2017-07-30 11:29:08 +0200

    extra header blade fixed

--- resources/views/themes/default/templates/partials/extra_header.blade.php
+++ resources/views/themes/default/templates/partials/extra_header.blade.php
@@ -1,10 +1,12 @@
 <script type="text/javascript">
+	{{--
 	var app_globals = {
 		home_url: "{{ url('/') }}",
 		api_url: "{{ url('/api') }}",
 		notify_url: "{{ route('api.notify.action') }}",
 		running_intervals:{}
 	};
+	--}}
 </script>
 <style type="text/css">
 
